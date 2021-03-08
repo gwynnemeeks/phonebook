64405926555b67c7e4abd63814c4c2340c047c57
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-03-08 11:16:11 -0600

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <v-app>
-    <v-app-bar app color="teal" dark>
+    <v-app-bar app color="pink" dark>
       <div class="d-flex align-center">
         <h3>Phonebook</h3>
       </div>
     </v-app-bar>
 
-    <v-main class="mt-5">
-      <!-- Stuff will go here -->
+    <v-main class="ma-5">
+      <phonebook />
     </v-main>
   </v-app>
 </template>
 
 <script>
-export default {};
+import Phonebook from "./components/Phonebook";
+
+export default {
+  components: {
+    Phonebook,
+  },
+};
 </script>
