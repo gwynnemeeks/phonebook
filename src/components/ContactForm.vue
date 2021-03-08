a065d03074a949e1401e786659125bafb04095ba
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-03-08 11:29:48 -0600

    form showing up and binding working

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -2,7 +2,11 @@
   <div>
     <h3 class="teal--text">New Contact</h3>
     <v-form>
-        <v-text-field outlined label="First Name" v-model="form.firstName" />
+      <v-text-field outlined label="First Name" v-model="form.firstName" />
+      <v-text-field outlined label="Last Name" v-model="form.lastName" />
+      <v-text-field type="number" outlined label="Phone" v-model="form.phone" />
+      <!-- TODO: phone type -->
+      <v-text-field outlined label="Email" v-model="form.email" />
     </v-form>
   </div>
 </template>
