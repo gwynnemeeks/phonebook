3209c2ee316f3bcca6a8b7fb35692a7522cf0696
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-03-10 02:59:07 -0600

    working wip

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,19 +1,38 @@
 <template>
   <div>
-    <h3 class="teal--text">New Contact</h3>
-    <v-form>
-      <v-text-field outlined label="First Name" v-model="form.firstName" />
-      <v-text-field outlined label="Last Name" v-model="form.lastName" />
-      <v-text-field type="number" outlined label="Phone" v-model="form.phone" />
-      <v-text-field outlined label="Email" v-model="form.email" />
+    <h2>New Contact</h2>
+    <v-form @submit.prevent="handleSubmit" ref="contactForm" class="mt-2 mr-3">
+      <v-text-field
+        outlined
+        :rules="validators.firstName"
+        label="First Name"
+        v-model="form.firstName"
+      ></v-text-field>
+      <v-text-field
+        outlined
+        :rules="validators.lastName"
+        label="Last Name"
+        v-model="form.lastName"
+      ></v-text-field>
+      <v-text-field
+        outlined
+        :rules="validators.phone"
+        label="Phone"
+        v-model="form.phone"
+      ></v-text-field>
       <v-select
         outlined
+        :items="options"
         label="Phone Type"
-        :items="phoneTypeOptions"
         v-model="form.type"
-      />
+      ></v-select>
+      <v-text-field
+        outlined
+        :rules="validators.email"
+        label="Email"
+        v-model="form.email"
+      ></v-text-field>
       <v-btn type="submit" color="pink" dark>Submit</v-btn>
-      <v-form @submit.prevent="handleSubmit"></v-form>
     </v-form>
   </div>
 </template>
@@ -29,22 +48,43 @@ export default {
         type: "",
         email: "",
       },
-      phoneTypeOptions: ["Home", "Cell", "Office"],
+      options: ["Home", "Office", "Cell"],
+      validators: {
+        firstName: [
+          (val) => !!val || "Contact first name is required",
+          (val) =>
+            val.length < 25 || "first name must be less than 25 characters",
+        ],
+        lastName: [
+          (val) =>
+            val.length < 25 || "first name must be less than 25 characters",
+        ],
+        phone: [(val) => val.length === 10 || "Enter valid phone number"],
+        email: [(val) => val.includes("@") || "Enter valid email"],
+      },
     };
-  }
-  };
+  },
   methods: {
     handleSubmit() {
+      //check validity
+      const isValid = this.$refs.contactForm.validate();
+
+      if (!isValid) {
+        return;
+      }
       this.$emit("contact-submit", this.form);
       this.form = {
         firstName: "",
         lastName: "",
         phone: "",
         type: "",
-        email: ""
-        };
-      }
-    }
+        email: "",
+      };
+
+      this.$refs.contactForm.resetValidation();
+    },
+  },
+};
 </script>
 
 <style></style>
