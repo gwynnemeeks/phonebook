7cc45b379fcc5f33038e069bccd5c9e6151ebcbe
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-03-10 09:56:40 -0600

    wip

--- src/components/Phonebook.vue
+++ src/components/Phonebook.vue
@@ -1,12 +1,16 @@
 <template>
-  <contact-form @contact-submit="addContact" />
+  <div>
+    <contacts-table :contacts="contacts" />
+    <contact-form @contact-submit="addContact" />
+  </div>
 </template>
 
 <script>
 import ContactForm from "./ContactForm.vue";
+import ContactsTable from "./ContactsTable.vue";
 
 export default {
-  components: { ContactForm },
+  components: { ContactForm, ContactsTable },
   data() {
     return {
       contacts: [],
